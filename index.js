6bff19e0294f6dc995f438cd38e9ba495c6861c1
alexandra <alexandraschurman@gmail.com>
2022-01-10 10:52:59 -0800

    commit tree 06e58772bb18ba6c54e22ad8f3d6f5725b4ccc3d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+
+
+const currentUser = 'Ali Schurman';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(); 
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const currentUser = 'Ali Schurman';
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
